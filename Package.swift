41410c2c4f5d371a689e0763bf074d23bbe8af50
杨波 <yangbo14@songguo7.com>
2025-11-01 16:05:02 +0800

    v3

--- Package.swift
+++ Package.swift
@@ -10,27 +10,16 @@ let package = Package(
         .iOS(.v14)
     ],
     products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "CYKit",
-            targets: ["CYKit"]
-        ),
-    ],
-    dependencies: [
-        // 你的其他依赖写这里
+            targets: ["CYKit"]),
     ],
     targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "CYKit",
-            dependencies: [],
-            swiftSettings: [
-                // ✅ 显式启用模拟器 arm64 支持
-                .unsafeFlags(["-target", "arm64-apple-ios14.0-simulator"], .when(platforms: [.iOS]))
-            ]
-        ),
-        .testTarget(
-            name: "CYKitTests",
-            dependencies: ["CYKit"]
-        ),
-    ],
-    swiftLanguageVersions: [.v5]
+            name: "CYKit"),
+
+    ]
 )
